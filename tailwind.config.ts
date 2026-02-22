cd501c548060a03ecd61cac0f6800deeebc174f9
gilliard09 <jefersonrocha998@gmail.com>
2026-02-21 21:19:12 -0300

    corrigindo erro dark

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors"; // Importe as cores padrão
 
 const config: Config = {
-    darkMode: ["class"],
+    darkMode: "class", // Correto
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
